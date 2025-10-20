ddd4f456635b1c2f3d2f7e50ccbc3a8bd60de62f
ZippyBonhTM <zippybont@gmail.com>
2025-10-20 13:32:54 -0300

    🧪 test: Move SUT creation to a factory helper method

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,21 +1,46 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 // sut = Sistem Under Test
 
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) { }
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
 interface CacheStore {
+  delete: () => void;
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
+
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+  return { cacheStore, sut };
+};
 
 describe('LocalSavePurchases', () => {
   test('Shold not delete cache on sut.init', () => {
-    const cacheStore = new CacheStoreSpy();
-    new LocalSavePurchases(cacheStore);
+    const { cacheStore } = makeSut();
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test('Shold delete old cache on sut.save', async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save()
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
