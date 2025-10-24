00ec5121dd8593de15ddf43f9146cb7c44e4edc0
ZippyBonhTM <zippybont@gmail.com>
2025-10-24 17:03:46 -0300

    🧪 test: ensure LocalSavePurchases throws if CacheStore.insert throws

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -24,6 +24,10 @@ class CacheStoreSpy implements CacheStore {
   simulateDeleteError(): void {
     jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error(); });
   }
+
+  simulateInsertError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error(); });
+  }
 }
 
 type SutTypes = {
@@ -77,5 +81,12 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.deleteCallsCount).toBe(1);
     expect(cacheStore.insertKey).toBe('purchases');
     expect(cacheStore.insertValues).toEqual(purchases);
+  });
+
+  test('Shold throw if insert throws', async () => {
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateInsertError();
+    const promice = sut.save(mockPurchases());
+    expect(promice).rejects.toThrow();
   });
 });
